9f08a6ccc87d5864bee95b50cb9536a7ee2738a0
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2024-06-21 19:33:27 +0700

    [FEAT] Menambahkan route dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthenticationController::class, 'index'])->name('/');
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 Route::resource('/pegawai', EmployeeController::class);
