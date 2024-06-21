e32a6ae21bb1e5521f87fc06d81f27f1703fc5df
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2024-06-21 19:35:01 +0700

    [FIX] Memperbaiki controller pegawai

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -13,7 +13,7 @@ class EmployeeController extends Controller
     public function index()
     {
         $employees = Employee::all();
-        return view('employees.index', compact('employees'));
+        return view('employee.index', compact('employees'));
     }
 
     /**
